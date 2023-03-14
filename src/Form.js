7f18406f324300b15819a33c5a019c5414a4c82b
Missfara <faluyi94@gmail.com>
2023-03-14 22:25:21 +0100

    added the html structure

---
+++ src/Form.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+let Form = () => {
+  return (
+    <form>
+      <label>
+        CARDHOLDER NAME <br />
+        <input type="text" placeholder="e.g Jane Appleseed" required />
+      </label>{" "}
+      <br /> <br />
+      <label>
+        CARD NUMBER <br />
+        <input type="text" placeholder="eg  0123 4567 8901 234000" required />
+      </label>
+      <br /> <br />
+      <div className="flex">
+        <label>
+          EXP.DATE (MM/YY) <br />
+          <input type="text" placeholder="MM" required  className="small"/>
+          <input type="text" placeholder="YY" required className="small" />
+        </label>
+
+        <label>
+          CVC <br />
+          <input type="text" />
+        </label>
+      </div> <br/> <br/>
+      <button>Confirm</button>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/Image.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+ let Image = () =>{
+    return(
+        <>
+      <img src="bg-main-desktop.png" alt="pic1" />
+      <img src="bg-card-front.png" alt="pic2" />
+      <img src="bg-card-back.png" alt="pic3" />
+      </>
+    )
+ }
+  export default Image
