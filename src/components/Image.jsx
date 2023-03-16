d71c9a1461bce73fd05a1c0aadd81511b37ce9bc
Faluyi Faramoluwa <faluyi94@gmail.com>
2023-03-17 00:43:06 +0100

    Changed the whole mobile setup and styling

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -5,25 +5,25 @@ let Image = () => {
   return (
     <div className={Imagecssmodule.outerdiv}>
       <div>{/* <img src="bg-main-desktop.png" alt="pic1" /> */}</div>
-      <div className={Imagecssmodule.imageonemobillediv}>
+      <div className={Imagecssmodule.bgContainer}>
         <img
           src="bg-main-mobile.png"
           alt="imageone-mobile"
-          className={Imagecssmodule.imageonemobille}
+          className={Imagecssmodule.bg}
         />
       </div>
-      <div className={Imagecssmodule.imagetwomobillediv}>
+      <div className={Imagecssmodule.backContainer}>
         <img
           src="bg-card-back.png"
           alt="imagetwo-mobile"
-          className={Imagecssmodule.imagetwomobille}
+          className={Imagecssmodule.back}
         />
       </div>
-      <div className={Imagecssmodule.imagethreemobilediv}>
+      <div className={Imagecssmodule.frontContainer}>
         <img
           src="bg-card-front.png"
           alt="imagethree-mobile"
-          className={Imagecssmodule.imagethreemobile}
+          className={Imagecssmodule.front}
         />
       </div>
     </div>
